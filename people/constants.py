62e8ef1cc092621779cfe0c1e80c6ea1b7cd3ab6
rlichiere <rlichiere@visiativ.com>
2018-08-10 05:58:42 +0200

    * cleanup

--- people/constants.py
+++ people/constants.py
@@ -1,17 +1,12 @@
 
 class _ParticipantPreferredMethodsOfContact(object):
-    PHONE_MOBILE = 'PHONE_MOBILE'
-    PHONE_STATIC = 'PHONE_STATIC'
-    EMAIL = 'EMAIL'
-    FACEBOOK = 'FACEBOOK'
+    PHONE_MOBILE = ('PHONE_MOBILE', 'Mobile phone')
+    PHONE_STATIC = ('PHONE_STATIC', 'Home phone')
+    EMAIL = ('EMAIL', 'Email')
+    FACEBOOK = ('FACEBOOK', 'Facebook')
 
     def as_choices(self):
-        return [
-            (self.PHONE_MOBILE, self.PHONE_MOBILE),
-            (self.PHONE_STATIC, self.PHONE_STATIC),
-            (self.EMAIL, self.EMAIL),
-            (self.FACEBOOK, self.FACEBOOK)
-        ]
+        return [self.PHONE_MOBILE, self.PHONE_STATIC, self.EMAIL, self.FACEBOOK]
 
 
 PARTICIPANT_PREFERRED_METHODS_OF_CONTACT = _ParticipantPreferredMethodsOfContact()
