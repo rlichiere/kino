59ee7a692b1ceaccd2dc1a83281650a17cb38a53
rlichiere <rlichiere@visiativ.com>
2018-08-13 04:47:05 +0200

    * added InterfaceModelLogger

--- core/generic/model.py
+++ core/generic/model.py
@@ -1,10 +1,11 @@
 
 from abc import abstractmethod
+import logging
 
 from django.utils.html import format_html
 
 
-class ModelRepresentation(object):
+class InterfaceModelRepresentation(object):
     """
         Exposes (NOT formatting BUT) rendering methods to integrated Models
     """
@@ -42,6 +43,61 @@ class ModelRepresentation(object):
         return '<a href="%s" target="_blank">%s</a>' % (_url, instance.__str__())
 
 
+class InterfaceModelLogger(object):
+    def __init__(self, logged_object, prefix=''):
+        self._logger = logging.getLogger('kino')
+        self._obj = logged_object
+        self._objectPrefix = '%s%s' % (self._obj.__module__, self._obj.__class__.__name__)
+        self._methodPrefix = prefix
+
+    """ PUBLIC """
+
+    def prepare(self, prefix):
+        """ MUST be called as first line of every object method """
+        self._methodPrefix = prefix
+
+    def debug(self, *args, **kwargs):
+        _msg = self._prepareLine(*args, **kwargs)
+        self._logger.debug(_msg)
+
+    def info(self, *args, **kwargs):
+        _msg = self._prepareLine(*args, **kwargs)
+        self._logger.info(_msg)
+
+    def warning(self, *args, **kwargs):
+        _msg = self._prepareLine(*args, **kwargs)
+        self._logger.warning(_msg)
+
+    def error(self, *args, **kwargs):
+        _msg = self._prepareLine(*args, **kwargs)
+        self._logger.error(_msg)
+
+    def exception(self, *args, **kwargs):
+        _msg = self._prepareLine(*args, **kwargs)
+        self._logger.exception(_msg)
+
+    """ PRIVATE """
+
+    def _prepareLine(self, *args, **kwargs):
+        # prepare prefix message
+        _res = '%s.%s' % (self._objectPrefix, self._methodPrefix)
+
+        # add args to message
+        for arg in args:
+            if _res != '':
+                _res += ' '
+            _res += str(arg)
+
+        # add kwargs to message
+        for kwarg, value in kwargs.iteritems():
+            if _res != '':
+                _res += ' '
+
+            _res += '%s : %s' % (kwarg, str(value))
+
+        return _res
+
+
 class GenericModelInterface(object):
     """
         Gives integrated tools to child Model classes
@@ -49,6 +105,7 @@ class GenericModelInterface(object):
 
     def __init__(self):
         self._representation = None
+        self.l = InterfaceModelLogger(logged_object=self)
 
     @abstractmethod
     def __str__(self):
@@ -73,5 +130,5 @@ class GenericModelInterface(object):
 
     def _get_or_instantiate_representation(self):
         if self._representation is None:
-            self._representation = ModelRepresentation(self)
+            self._representation = InterfaceModelRepresentation(self)
         return self._representation
